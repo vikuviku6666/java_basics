5968146d5746f9c1120828264de48c80b4ce0c1e
Vinay <vinay8888@gmail.com>
2023-12-29 01:27:07 +0100

    second commit

--- src/com/vinkun/Examples.java
+++ src/com/vinkun/Examples.java
@@ -4,7 +4,7 @@ public class Examples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Vina");
+		System.out.println("Hello Vinay");
 
 	}
 
